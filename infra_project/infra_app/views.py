9d14f35dc01d36b65effe08d2cef7c32ae4b2c1b
Ignat Dan <igntdan777@gmail.com>
2022-06-20 12:36:33 +0800

    final

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Едем на рынок!!!')
 
 
 def second_page(request):
